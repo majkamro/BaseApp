162747905703cae69d4f461bacb16032fe39e963
Majid Kholghi Shirazi <majkamro@googlemail.com>
2022-08-05 22:25:34 +0430

    Change Projects Architectures

--- BaseApp/Controllers/HomeController.cs
+++ BaseApp/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using BaseApp.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BaseApp.Controllers
 {
@@ -16,17 +14,6 @@ namespace BaseApp.Controllers
 		public IActionResult Index()
 		{
 			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
 	}
 }
